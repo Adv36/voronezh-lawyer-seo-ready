f74811454a849427b363e05bd0714ac1d4e488a2
Adv36 <ripinsky@list.ru>
2026-03-06 23:21:46 +0300

    Update page.tsx

--- voronezh-lawyer-main/src/app/contact/page.tsx
+++ voronezh-lawyer-main/src/app/contact/page.tsx
@@ -1,17 +1,11 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: "Контакты",
-  description: "Связаться с адвокатом",
-};
 
 export default function ContactPage() {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <Header />
-      <main>Контакты</main>
+      <main className="flex-grow">Contact</main>
       <Footer />
     </div>
   );
