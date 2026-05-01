d8b4cb3f1e652aea0ca0b0ccf6958bcbfd2b6ae6
Adv36 <ripinsky@list.ru>
2026-05-01 15:44:04 +0300

    Update page.tsx
    fix: add jsconfig for @ alias resolution

--- voronezh-lawyer-main/src/app/about/page.tsx
+++ voronezh-lawyer-main/src/app/about/page.tsx
@@ -1,12 +1,12 @@
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import AboutContent from '../components/About';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+import AboutContent from '@/components/About';
 
 export default function AboutPage() {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="flex-grow">About</main>
+      <main className="flex-grow">Обо мне</main>
       <Footer />
     </div>
   );
