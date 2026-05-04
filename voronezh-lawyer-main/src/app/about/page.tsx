859ca4d700f08b8af0a3a92a8427f669ee90fff5
Adv36 <ripinsky@list.ru>
2026-05-04 12:27:56 +0300

    Update page.tsx

--- voronezh-lawyer-main/src/app/about/page.tsx
+++ voronezh-lawyer-main/src/app/about/page.tsx
@@ -1,6 +1,6 @@
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
-import AboutContent from '@/components/About';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import AboutContent from '../../components/About';
 
 export default function AboutPage() {
   return (
