59a7d434de9cd3edb15fde792e3bb38bd1d9c0cd
Adv36 <ripinsky@list.ru>
2026-05-06 17:43:31 +0300

    fix: update package.json and contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -32,7 +32,7 @@ export default function ContactPage() {
             <input type="hidden" name="form-name" value="contact" />
             <p className="hidden">
               <label>
-                Don’t fill this out if you’re human: <input name="bot-field" />
+                Don't fill this out if you're human: <input name="bot-field" />
               </label>
             </p>
 
